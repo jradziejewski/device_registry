3b2a6a35f5f64e79a2d8761c298625a742b25325
jradziejewski <192462@student.ue.wroc.pl>
2024-10-16 16:35:53 +0200

    feat: implement preventing registering device to another user

--- app/services/assign_device_to_user.rb
+++ app/services/assign_device_to_user.rb
@@ -2,8 +2,13 @@
 
 class AssignDeviceToUser
   def initialize(requesting_user:, serial_number:, new_device_owner_id:)
+    @requesting_user = requesting_user
+    @serial_number = serial_number
+    @new_device_owner_id = new_device_owner_id
   end
 
   def call
+    device = Device.find_by(serial_number: @serial_number)
+    raise RegistrationError::Unauthorized if @requesting_user.id != @new_device_owner_id
   end
 end
