22e0761af88eabc799a446d5d965a05a6b68fccf
jradziejewski <192462@student.ue.wroc.pl>
2024-10-17 12:54:53 +0200

    feat: handle assigning error in device controller

--- app/errors/assigning_error.rb
+++ app/errors/assigning_error.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 class AssigningError < StandardError
-  class AlreadyUsedOnOtherUser < self; end
-  class AlreadyUsedOnUser < self; end
+  class AlreadyUsedOnOtherUser < self
+    def initialize
+      super('Device is already assigned to another user')
+    end
+  end
+  class AlreadyUsedOnUser < self
+    def initialize
+      super('Device was previously assigned to the user')
+    end
+  end
 end
