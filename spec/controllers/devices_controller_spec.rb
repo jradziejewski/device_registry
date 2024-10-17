843d2a43d42d50e1951f3e1b2eefbaf8d442caa7
jradziejewski <192462@student.ue.wroc.pl>
2024-10-17 13:12:23 +0200

    feat: add unassign action to devices controller & test case

--- spec/controllers/devices_controller_spec.rb
+++ spec/controllers/devices_controller_spec.rb
@@ -41,6 +41,48 @@ RSpec.describe DevicesController, type: :controller do
   end
 
   describe 'POST #unassign' do
-    # TODO: implement the tests for the unassign action
+    subject(:unassign) do
+      post :unassign,
+           params: { serial_number: '123456', target_user_id: assigned_user_id },
+           session: { token: user.api_keys.first.token }
+    end
+
+    context 'when the user is authenticated' do
+      context 'when user tries to unassign a device that belongs to another user' do
+        let(:assigned_user) { create(:user, email: 'other_user@example.com') }
+        let(:assigned_user_id) { assigned_user.id }
+
+        before do
+          AssignDeviceToUser.new(
+            requesting_user: assigned_user,
+            serial_number: '123456',
+            new_device_owner_id: assigned_user_id
+          ).call
+        end
+
+        it 'returns an unauthorized response' do
+          unassign
+          expect(response.code.to_i).to eq(401)
+          expect(JSON.parse(response.body)).to eq({ 'error' => ReturnError::Unauthorized.new.message })
+        end
+      end
+
+      context 'when user tries to unassign a device that belongs to self' do
+        let(:assigned_user_id) { user.id }
+
+        before do
+          AssignDeviceToUser.new(
+            requesting_user: user,
+            serial_number: '123456',
+            new_device_owner_id: assigned_user_id
+          ).call
+        end
+
+        it 'returns a success response' do
+          unassign
+          expect(response).to be_successful
+        end
+      end
+    end
   end
 end
