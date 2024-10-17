ff6caa375de840103583f84e5f0e1c3c2559b014
jradziejewski <192462@student.ue.wroc.pl>
2024-10-17 13:07:05 +0200

    fix: add check if requesting user is the same as assigned user to return device from user service

--- app/services/return_device_from_user.rb
+++ app/services/return_device_from_user.rb
@@ -8,11 +8,13 @@ class ReturnDeviceFromUser
   end
 
   def call
+    raise ReturnError::Unauthorized if @requesting_user.id != @assigned_user_id
+
     device = Device.find_by(serial_number: @serial_number)
 
     raise ReturnError::DeviceNotFound if device.nil?
     raise ReturnError::DeviceAlreadyReturned if device.user.nil?
-    raise ReturnError::Unauthorized unless device.user.id == @assigned_user_id
+    raise ReturnError::Unauthorized if device.user.id != @assigned_user_id
 
     device.update!(user: nil)
 
