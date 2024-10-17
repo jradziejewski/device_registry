b018343b49ed403a907bd7986dd712f9685c3af1
jradziejewski <192462@student.ue.wroc.pl>
2024-10-17 13:38:20 +0200

    feat: add user model validations & test

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,16 @@ class User < ApplicationRecord
   has_many :devices
   has_many :api_keys, as: :bearer
   has_secure_password
+
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true, if: -> { password.present? }
+
+  validate :passwords_must_match
+
+  private
+
+  def passwords_must_match
+    errors.add(:password, 'must match password confirmation') unless password == password_confirmation
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,42 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  unique_email = 'unique@example.com'
+  password = 'password'
+
+  it 'is valid with valid attributes' do
+    user = User.new(email: unique_email, password: password, password_confirmation: password)
+    expect(user).to be_valid
+  end
+
+  it 'validates presence of email' do
+    user = User.new(email: nil, password: password, password_confirmation: password)
+    expect(user.valid?).to be_falsey
+    expect(user.errors[:email]).to include("can't be blank")
+  end
+
+  it 'validates uniqueness of email' do
+    create(:user, email: unique_email)
+    user = User.new(email: unique_email, password: password, password_confirmation: password)
+    expect(user.valid?).to be_falsey
+    expect(user.errors[:email]).to include('has already been taken')
+  end
+
+  it 'validates presence of password' do
+    user = User.new(email:unique_email, password: nil, password_confirmation: password) 
+    expect(user.valid?).to be_falsey
+    expect(user.errors[:password]).to include("can't be blank")
+  end
+
+  it 'validates presence of password_confirmation' do
+    user = User.new(email:unique_email, password: password, password_confirmation: nil)
+    expect(user.valid?).to be_falsey
+    expect(user.errors[:password_confirmation]).to include("can't be blank")
+  end
+
+  it 'validates that password matches password_confirmation' do
+    user = User.new(email:unique_email, password: password, password_confirmation: 'notpassword')
+    expect(user.valid?).to be_falsey
+    expect(user.errors[:password]).to include('must match password confirmation')
+  end
 end
